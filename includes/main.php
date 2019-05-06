4ae06596df51cc9d044f57c5329b6d4cf63d4559
Emi <empusdev@gmail.com>
2019-05-06 17:04:56 +0200

    17h04

--- includes/main.php
+++ includes/main.php
@@ -2,11 +2,7 @@
 
 $content = glob('./includes/*.inc.php');
 $page = isset($_GET['page']) ? $_GET['page'] : "";
+$page = "./includes/" . $page . ".inc.php";
 
-$page = "./includes/" . $page . "inc.php";
-if (in_array($page, $content)) {
-  require $page;
-}
-else {
-  require './includes/home.inc.php';
-}
+$page = in_array($page, $content) ? $page : '.includes/home.inc.php';
+require $page;
