77cd2f738cf260cbb4c564b9d456590c726343c0
Emi <empusdev@gmail.com>
2019-05-06 15:56:31 +0200

    header footer main et index

---
+++ includes/header.php
@@ -0,0 +1,6 @@
+<nav>
+  <ul>
+    <li><a href="index.php?page=home" title="Page d'accueil">Accueil</a></li>
+      <li><a href="index.php?page=article" title="Articles">Actualités</a></li>
+  </ul>
+</nav>
--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Site de test PHP</title>
+    <link rel="stylesheet" href="global.css">
+  </head>
+  <body>
+  <header>
+    <?php include('includes/header.php'); ?>
+  </header>
+    <?php include('includes/main.php'); ?>
+    <footer>
+      <?php include('includes/footer.php'); ?>
+    </footer>
+  </body>
+</html>
