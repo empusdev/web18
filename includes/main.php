f161bde2c3892b9615c873fa8aec6ee2ef4917cb
Emi <empusdev@gmail.com>
2019-05-06 17:26:13 +0200

    17h26

--- includes/main.php
+++ includes/main.php
@@ -3,6 +3,6 @@
 $content = glob('./includes/*.inc.php');
 $page = isset($_GET['page']) ? $_GET['page'] : "";
 $page = "./includes/" . $page . ".inc.php";
-
-$page = in_array($page, $content) ? $page : '.includes/home.inc.php';
+//On vérifie si la chaîne précédente est présente dans le tableau renvoyé
+$page = in_array($page, $content) ? $page : './includes/home.inc.php';
 require $page;
