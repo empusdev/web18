91367a8f03184215db46b11e751c20f65603021a
Emi <empusdev@gmail.com>
2019-05-06 16:56:27 +0200

    16h56

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,9 @@
 <nav>
   <ul>
     <li><a href="index.php?page=home" title="Page d'accueil">Accueil</a></li>
-      <li><a href="index.php?page=article" title="Articles">Actualités</a></li>
+      <li><a href="index.php?page=news" title="Articles">Actualités</a></li>
+        <li><a href="index.php?page=contact" title="Contact">Contact</a></li>
+          <li><a href="index.php?page=inscription" title="Inscription">Inscription</a></li>
+            <li><a href="index.php?page=connexion" title="Connexion">Connexion</a></li>
   </ul>
 </nav>
--- includes/main.php
+++ includes/main.php
@@ -1,1 +1,12 @@
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<?php
+
+$content = glob('./includes/*.inc.php');
+$page = isset($_GET['page']) ? $_GET['page'] : "";
+
+$page = "./includes/" . $page . "inc.php";
+if (in_array($page, $content)) {
+  require $page;
+}
+else {
+  require './includes/home.inc.php';
+}
