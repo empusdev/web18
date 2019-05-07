7fad4d5cff964aabc3c406cbc63c75e5b81a286e
Emi <empusdev@gmail.com>
2019-05-07 12:12:29 +0200

    mar7 12:12

--- includes/formregistration.php
+++ includes/formregistration.php
@@ -1,24 +1,24 @@
 <form action="index.php?page=registration" method="post">
   <div>
     <label for="nom">Nom&nbsp;:</label>
-    <input type="text" name="nom" placeholder="Nom" required />
+    <input type="text" name="nom" placeholder="Nom" />
   </div>
     <div>
       <label for="prenom">Prénom&nbsp;:</label>
-      <input type="text" name="prenom" placeholder="Prénom" required />
+      <input type="text" name="prenom" placeholder="Prénom" />
     </div>
       <div>
         <label for="email">E-mail&nbsp;:</label>
-        <input type="mail" name="email" placeholder="adresse e-mail" required />
+        <input type="mail" name="email" placeholder="adresse e-mail" />
       </div>
         <div>
           <label for="passwd">Mot de passe&nbsp;:</label>
-          <input type="password" name="passwd" required />
+          <input type="password" name="passwd" />
         </div>
 
         <div>
-          <input type="reset" value="Effacer"/>
-            <input type="submit" value="Envoyer"/>
+          <input type="reset" value="Effacer" class="sub"/>
+            <input type="submit" value="Envoyer" class="sub" />
         </div>
         <input type="hidden" name="validation" />
 </form>
--- includes/registration.inc.php
+++ includes/registration.inc.php
@@ -1,2 +1,30 @@
 <?php
-require_once 'formregistration.php';
+if (isset($_POST['validation'])) {
+  // Récupération des éléments du formulaire
+  $nom = isset($_POST['nom']) ? $_POST['nom'] : "";
+    $prenom = isset($_POST['prenom']) ? $_POST['prenom'] : "";
+      $email = isset($_POST['email']) ? $_POST['email'] : "";
+        $mdp = isset($_POST['passwd']) ? $_POST['passwd'] : "";
+
+  // Initialisation d'un tableau vide
+  $erreur = array();
+  if ($nom == "") { array_push($erreur, "Veuillez saisir un nom"); } //ajout d'une erreur dans le tableau
+  if ($prenom == "") { array_push($erreur, "Veuillez saisir un prénom"); }
+  if ($email == "") { array_push($erreur, "Veuillez saisir un mail"); }
+  if ($mdp == "") { array_push($erreur, "Veuillez saisir un mot de passe"); }
+
+  //On vérifie si le tableau d'erreurs est vide ou non
+  if (count($erreur) > 0) {
+    echo "<ul>";
+    for ($i=0; $i<(count($erreur)) ; $i++) {
+      echo "<li>".$erreur[$i]."</li>";
+    }
+    echo "</ul>";
+  }
+  else {
+    echo "Le tableau est vide, insertion en BDD";
+  }
+}
+else {
+  require_once 'formregistration.php';
+}
